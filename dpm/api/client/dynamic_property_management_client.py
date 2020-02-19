06ea3a5d08c9e419b3a8ea61f968b799fb6460be
jspiropulo <spiropulo@gmail.com>
2020-02-18 17:07:16 -0800

    Ready to call the service.

---
+++ dpm/api/client/dynamic_property_management_client.py
@@ -0,0 +1,8 @@
+from typing import Dict
+
+import requests
+
+class DynamicPropertyManagementClient:
+    def get_dynamic_properties(self, env: str, service_id: str) -> Dict[str, str]:
+        #requests.post()
+        pass
