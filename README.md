e86a041cd0d2c2bd00a34f026afdd9a39c03c573
jspiropulo <spiropulo@gmail.com>
2020-03-24 14:58:01 -0700

    Passing all tests and cleaner API.

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
-Use this module for dynamic properties.
 
 
-Must set:
+### How to install:
 
-* DYNAMIC_PROPERTIES_URL
-* USERNAME
-* PASSWORD
-* DYNAMIC_PROPERTIES_PULL_DELAY
+for pipenv
+
+```
+[packages]
+dynamic-property-management-module = { version = "*", git = "https://github.com/mozilla-it/dynamic-property-management-module.git", ref = "master"}
+
+```
+
+### Must be set:
+These environmental variables must be set.
+* GOOGLE_APPLICATION_CREDENTIALS
+* GCP_PROJECT
+
+### Example Usage:
+
+```
+**[Pulling Properties]**
+
+Env.initialize(
+               dpm_service_name = <service name/>, 
+               dpm_program_name = <program_name/>,
+               dpm_polling_interval = <dpm_polling_interval/>,
+               secrets_name = <secrets_name/>, 
+               secrets_polling_interval = <secrets_polling_interval/>,
+               project = <project/>
+              )
+property_value = Env.get_property(<property_name/>)
+
+**[Pulling Secrets]**
+
+Env.initialize(
+               dpm_service_name = <service name/>, 
+               dpm_program_name = <program_name/>,
+               dpm_polling_interval = <dpm_polling_interval/>,
+               secrets_name = <secrets_name/>, 
+               secrets_polling_interval = <secrets_polling_interval/>,
+               project = <project/>
+              )
+secret_value = Env.get_secret(<secret_name/>)
+```
---
+++ dpm/api/env.py
@@ -0,0 +1,47 @@
+from dpm.api.clients import DynamicPropertyManagementClient, SecretsClient
+import logging
+import json
+
+
+class Env:
+    logger = logging.getLogger(__name__)
+    dpm_service_name: str = None
+    dpm_program_name: str = None
+    secrets_name: str = None
+    dpm_polling_interval: int = 0
+    secrets_polling_interval: int = 0
+    dpm_client: DynamicPropertyManagementClient = None
+    secrets_client: SecretsClient = None
+    initialized: bool = False
+
+    @staticmethod
+    def initialize(dpm_service_name: str, dpm_program_name: str, secrets_name: str, project: str,
+                   dpm_polling_interval: int = 300, secrets_polling_interval: int = 0):
+
+        Env.dpm_service_name = dpm_service_name
+        Env.dpm_program_name = dpm_program_name
+        Env.secrets_name = secrets_name
+        Env.dpm_polling_interval = dpm_polling_interval
+        Env.dpm_client = DynamicPropertyManagementClient(
+            service_name=dpm_service_name, program_name=dpm_program_name, polling_interval=dpm_polling_interval, project=project)
+        Env.secrets_client = SecretsClient(secrets_name=secrets_name, polling_interval=secrets_polling_interval, project=project)
+        Env.initialized = True
+        Env.logger.info(f"Initializing with dpm_service_name={dpm_service_name}, "
+                        f"dpm_program_name={dpm_program_name}, "
+                        f"secrets_name={secrets_name}, project={project}, "
+                        f"dpm_polling_interval={dpm_polling_interval}, "
+                        f"secrets_polling_interval={secrets_polling_interval}")
+
+    @staticmethod
+    def get_property(key: str) -> str:
+        if Env.initialized is False:
+            Env.logger.error("Trying to get_property without initialization")
+            raise Exception("You must invoke Env.initialize with the appropriate parameters")
+        return Env.dpm_client.get_dynamic_properties().get(key)
+
+    @staticmethod
+    def get_secret(key: str) -> str:
+        if Env.initialized is False:
+            Env.logger.error("Trying to get_secret without initialization")
+            raise Exception("You must invoke Env.initialize with the appropriate parameters")
+        return Env.secrets_client.get_secret(key)
