c15d737eb9a4cb893e93291b071f9e7dd77e9151
jspiropulo <spiropulo@gmail.com>
2020-04-30 16:06:21 -0700

    Moving code for proper macking.

--- dpm/api/env.py
+++ dpm/api/env.py
@@ -1,4 +1,4 @@
-from dpm.api.clients import DynamicPropertyManagementClient, SecretsClient
+import dpm.api.clients
 import logging
 
 
@@ -8,8 +8,8 @@ class Env:
     dpm_program_name: str = None
     secrets_name: str = None
     secrets_polling_interval: int = 0
-    dpm_client: DynamicPropertyManagementClient = None
-    secrets_client: SecretsClient = None
+    dpm_client: dpm.api.clients.DynamicPropertyManagementClient = None
+    secrets_client: dpm.api.clients.SecretsClient = None
     dpm_initialized: bool = False
     secrets_initialized: bool = False
 
@@ -20,14 +20,14 @@ class Env:
         # Setting up dynamic properties
         Env.dpm_service_name = dpm_service_name
         Env.dpm_program_name = dpm_program_name
-        Env.dpm_client = DynamicPropertyManagementClient(
+        Env.dpm_client = dpm.api.clients.DynamicPropertyManagementClient(
             service_name=dpm_service_name, program_name=dpm_program_name)
         Env.dpm_initialized = True
 
         # Setting up secrets manager
         if secrets_name is not None and project is not None:
             Env.secrets_name = secrets_name
-            Env.secrets_client = SecretsClient(secrets_name=secrets_name, polling_interval=secrets_polling_interval, project=project)
+            Env.secrets_client = dpm.api.clients.SecretsClient(secrets_name=secrets_name, polling_interval=secrets_polling_interval, project=project)
             Env.secrets_initialized = True
             Env.logger.info(f"Initializing with dpm_service_name={dpm_service_name}, "
                             f"dpm_program_name={dpm_program_name}, "
