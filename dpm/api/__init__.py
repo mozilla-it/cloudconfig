b4483ca1e90428fd3b4c052784c13947be73d84a
Bryan Sieber <bsieber@mozilla.com>
2020-06-05 10:53:16 -0700

    Adding __all__

--- dpm/api/__init__.py
+++ dpm/api/__init__.py
@@ -2,3 +2,5 @@ from .clients import DynamicPropertyManagementClient
 from .clients import SecretsClient
 from .env import Env
 from .seed import DynamicPropertiesSeeder
+
+__all__ = ["DynamicPropertyManagementClient", "SecretsClient", "Env", "DynamicPropertiesSeeder"]
