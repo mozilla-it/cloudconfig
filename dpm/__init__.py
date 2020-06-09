0d24ce843e7137bb19773842618f5904be594f50
Bryan Sieber <bsieber@mozilla.com>
2020-06-08 20:14:14 -0700

    Initializing for training through

---
+++ dpm/__init__.py
@@ -0,0 +1,2 @@
+from .api import *
+__all__ = ["api"]
---
+++ dpm/api/__init__.py
@@ -0,0 +1,6 @@
+from .clients import DynamicPropertyManagementClient
+from .clients import SecretsClient
+from .seed import DynamicPropertiesSeeder
+from .env import Env
+
+__all__ =["DynamicPropertyManagementClient" , "SecretsClient", "DynamicPropertiesSeeder", "Env"]
