06682ea6744a22e6e032bb53eec9966e3eec78e2
Chris Valaas <cvalaas@mozilla.com>
2020-07-22 11:44:36 -0700

    test_seed

---
+++ dpm/tests/unit/test_seed.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+
+
+def test_dynamic_property_seeder(monkeypatch):
+    import dpm.api.seed
+    from dpm.api.seed import DynamicPropertiesSeeder
+
+    test_props = {}
+
+    def mock_initialize(*args, **kwargs):
+        return None
+
+    def mock_insert(*args, **kwargs):
+        # global test_props
+        test_props[args[0]] = args[1]
+
+    monkeypatch.setattr(dpm.api.seed.Env, "initialize", mock_initialize)
+    monkeypatch.setattr(dpm.api.seed.Env, "insert_property", mock_insert)
+
+    seeder = DynamicPropertiesSeeder(
+        "prod", "dpm_service_name", "dpm/tests/resource/dynamic-properties-seed/"
+    )
+    seeder.execute()
+    # print(test_props)
+    assert test_props == json.loads(
+        open("dpm/tests/resource/dynamic-properties-seed/prod/partner_a.json").read()
+    )
