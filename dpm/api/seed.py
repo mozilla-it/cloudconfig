396db42855356fbb550aa276846266aed3a697f9
jspiropulo <spiropulo@gmail.com>
2020-07-22 16:27:16 -0700

    Simplifying seeding util we change static structures.

--- dpm/api/seed.py
+++ dpm/api/seed.py
@@ -6,15 +6,15 @@ from .env import Env
 
 
 class DynamicPropertiesSeeder:
-    def __init__(self, env: str, dpm_service_name: str, seeds_path: str):
+    def __init__(self, env: str, dpm_service_name: str, seeds_path: str, dpm_program_name: str):
         self.env = env
         self.dpm_service_name = dpm_service_name
         self.seeds_path = seeds_path
+        self.dpm_program_name = dpm_program_name
 
     def execute(self):
         for filename in glob.glob(f"{self.seeds_path}/{self.env}/*.json"):
-            dpm_program_name = os.path.basename(filename).replace('.json', '')
-            Env.initialize(dpm_service_name=self.dpm_service_name, dpm_program_name=dpm_program_name)
+            Env.initialize(dpm_service_name=self.dpm_service_name, dpm_program_name=self.dpm_program_name)
             content = json.loads(open(filename).read())
             keys = content.keys()
             for key in keys:
